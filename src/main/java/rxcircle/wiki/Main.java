c537a609361c7e0145dcd72f21f6c974ba6c176a
Madeleine Jöngren <madeleine.jongren@gmail.com>
2016-03-26 18:19:42 +0100

    Layout first version

--- src/main/java/rxcircle/wiki/Main.java
+++ src/main/java/rxcircle/wiki/Main.java
@@ -1,12 +1,23 @@
 package rxcircle.wiki;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
 import javafx.stage.Stage;
+
+import java.util.Date;
 
 import static javafx.application.Application.launch;
 
@@ -19,26 +30,58 @@ public class Main extends Application {
     private static final String SEARCH_URL = "https://sv.wikipedia.org/w/api.php?action=query&list=search&srsearch={query}&format=json&utf8=";
     private static final String ARTICLE_URL = "https://sv.wikipedia.org/w/api.php?action=query&titles={title}&prop=revisions&rvprop=content&format=json&utf8=";
 
+    public static final ObservableList data =
+            FXCollections.observableArrayList();
+
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("Hello World!");
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
+
+        primaryStage.setTitle("List View Sample");
+
+        final ListView listView = new ListView(data);
+        listView.setPrefSize(200, 250);
+        listView.setEditable(false);
+
+        data.add("First hit");
+        data.add("Second hit");
+
+        Label searchLabel = new Label("Search:");
+        TextField searchField = new TextField();
+
+        Button searchButton = new Button();
+        searchButton.setText("Say 'Hello World'");
+        searchButton.setOnAction(new EventHandler<ActionEvent>() {
 
             @Override
             public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+                data.add("Hej: " + new Date().getTime());
             }
         });
 
+        WebView  browser = new WebView();
+        WebEngine engine = browser.getEngine();
+        String url = "http://www.wikipedia.com/";
+        engine.load(url);
+
+        BorderPane mainBorderPane = new BorderPane();
+        BorderPane searchBorderPane = new BorderPane();
+        mainBorderPane.setLeft(searchBorderPane);
+        mainBorderPane.setCenter(browser);
+
+        HBox hb = new HBox();
+        hb.getChildren().addAll(searchLabel, searchField, searchButton);
+        hb.setSpacing(10);
+
+        searchBorderPane.setLeft(listView);
+        searchBorderPane.setTop(hb);
+
         StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        primaryStage.setScene(new Scene(root, 300, 250));
+        root.getChildren().add(mainBorderPane);
+        primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
     }
 }
